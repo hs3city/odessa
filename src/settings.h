460c53782c8293eeb5f9eb027e01d08c0a87aa67
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2023-10-19 16:53:39 +0200

    Switch to MH-ET Live Mini32

--- src/settings.h
+++ src/settings.h
@@ -2,7 +2,9 @@
 const char *ssid     = "Your SSID goes here";
 const char *password = "AVerySafePassw0rd";
 
-const char* BROKER = "X.Y.Z.W";
-const uint16_t BROKER_PORT = 1883;
+const char*    MQTT_BROKER      = "X.Y.Z.W";
+const uint16_t MQTT_BROKER_PORT = 1883;
+const char*    MQTT_USER        = "username";
+const char*    MQTT_PASSWORD    = "password";
 
 const String topic="feed/public_transport";
